aa9e77e4d7a61c0e088534986dfa0f252fc37fc2
Sandy Gomez <sgomez1029@gmail.com>
2025-06-19 16:12:31 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sandra Gomez on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
