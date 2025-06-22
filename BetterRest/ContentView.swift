8ddc768867ab5152eb002fa5002fee2c23405fa3
Sandy Gomez <sgomez1029@gmail.com>
2025-06-22 13:11:21 -0700

    Day 27 - Building a Basic Layout

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,18 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationStack {
+            VStack {
+                Text ("When do you want to wake up")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted())hours", value: $sleepAmount, in: 4...12, step: 0.25)
+
+                Text("Daily coffee intake")
+                
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button ("Calculate", action: calculatedBedtime)
+            }
+        }
     }
-    func exampleDates() {
-//        var components = DateComponents()
-//        componenets.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? .now
+    
+    func calculatedBedtime() {
         
-        var components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
     }
 }
 
