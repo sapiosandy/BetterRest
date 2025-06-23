b68253fe7b632207499dfe97d62477acb6a77f14
Sandy Gomez <sgomez1029@gmail.com>
2025-06-23 15:51:36 -0700

    Connecting SwiftUI to Core ML

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Sandra Gomez on 6/19/25.
 //
-
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +12,10 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showAlert = false
     
     var body: some View {
         NavigationStack {
@@ -32,15 +36,38 @@ struct ContentView: View {
             }
             .navigationTitle("Better Rest")
             .toolbar {
-                Button ("Calculate", action: calculatedBedtime)
+                Button ("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
             }
         }
     }
     
-    func calculatedBedtime() {
-        
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Int64(Double(hour + minute)), estimatedSleep: sleepAmount, coffee: Int64(Double(coffeeAmount)))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime"
+        }
     }
 }
+
 
 #Preview {
     ContentView()
